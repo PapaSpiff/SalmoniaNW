a544a8ba16bcecf42fa4a032b7e6aaa773dc9cc9
PapaSpiff <papaspiff.spl@gmail.com>
2024-04-20 15:06:50 +0200

    use new source for hash generation

--- genhash.sh
+++ genhash.sh
@@ -2,7 +2,8 @@
 
 FNAME=hash.py
 #HASHREF=https://raw.githubusercontent.com/nintendoapis/splatnet3-types/main/src/generated/latest.ts
-HASHREF=https://gitlab.fancy.org.uk/samuel/splatnet3-types/-/raw/main/src/generated/latest.ts
+#HASHREF=https://gitlab.fancy.org.uk/samuel/splatnet3-types/-/raw/main/src/generated/latest.ts
+HASHREF=https://raw.githubusercontent.com/misenhower/splatoon3.ink/main/app/splatnet/queryHashes.json
 
 cat > $FNAME <<EOF
 from enum import Enum
@@ -13,7 +14,8 @@ from enum import Enum
 class SHA256Hash(Enum):
 EOF
 
-curl -s $HASHREF | cut -f5,8 -d ' ' | sed s,.\./,\ =\ \", | sed 's/\.js../\"/' | sed 's,queries/,,' |  sed 's/^/        /' >> $FNAME
+#curl -s $HASHREF | cut -f5,8 -d ' ' | sed s,.\./,\ =\ \", | sed 's/\.js../\"/' | sed 's,queries/,,' |  sed 's/^/        /' >> $FNAME
+curl -s $HASHREF | grep "^      " | sed 's/^\( *\)\"\([^\"]*\)\"/\1\2/' | sed 'y/:/=/' | sed 's/,$//' | sed 's/^/  /' >> $FNAME
 
 cat >> $FNAME <<EOF
 
